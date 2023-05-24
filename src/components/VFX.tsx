1a1eebfeeef761f10de4f0d8c5759dd2e534ca60
Geeknerd1337 <geeknerd1337@gmail.com>
2023-05-24 01:26:36 -0500

    Fix more broken links

--- src/components/VFX.tsx
+++ src/components/VFX.tsx
@@ -21,11 +21,13 @@ export default function VFX() {
         title="Dot Com Guy"
         description="Dot Com Guy is a sequel to the music video HD ME. A test of various VFX skills such as 3D tracking, 3D modeling, and compositing. Often described as weirdly compelling."
         image="dcg.png"
+        link="https://www.youtube.com/watch?v=8WPKzuzy9UQ"
       />
       <ProductCard
         title="AI: The Modern Eldritch Horror"
         description="A student short film I made for my computer ethics class in school. It shows of my skills using video editing programs like Sony Vegas and Davinci resolve. It covers the many dangerous associated with generative AI and the potential future it may lead us to."
         image="ai.png"
+        link="https://www.youtube.com/watch?v=Ik_da_xMPzE"
       />
     </VFXHolder>
   );
